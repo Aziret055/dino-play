2953789bf4dbe88e482c77a923789be9566e6162
Aziret055 <kojom5erd1ev1@gmail.com>
2023-10-11 16:02:49 +0600

    dino

--- scrip.js
+++ scrip.js
@@ -18,12 +18,12 @@ dino.classList.remove("jump")
   },300)
 }
 
-// let isAlive = setInterval(function() {
-//   let dinoTop = parseInt(window.getComputedStyle(dino).getPropertyValue("top"))
-//   let cactusLeft = parseInt(window.getComputedStyle(cactus).getPropertyValue("left"))
-//   if(cactusLeft < 50 && cactusLeft > 0 && dinoTop >= 140 ) {
-//     alert("Ты лоX")
-//   }
-// },10)
+let isAlive = setInterval(function() {
+  let dinoTop = parseInt(window.getComputedStyle(dino).getPropertyValue("top"))
+  let cactusLeft = parseInt(window.getComputedStyle(cactus).getPropertyValue("left"))
+  if(cactusLeft < 50 && cactusLeft > 0 && dinoTop >= 140 ) {
+    alert("Ты лоX")
+  }
+},10)
 
 
